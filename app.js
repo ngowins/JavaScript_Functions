2038d4dcf18d076e27c24fc3aa11910f8c6e5445
Nate Gowins <ngowins@gmail.com>
2022-03-30 14:52:34 -0500

    added extra exercises

--- app.js
+++ app.js
@@ -34,3 +34,48 @@ function checkAge(userName, age) {
 }
 
 checkAge(userName, age);
+
+Exercise 3 Section
+console.log("EXERCISE 3:\n==========");
+
+let x = prompt("Enter your 'X' coordinate");
+let y = prompt("Enter your 'Y' coordinate");
+
+function quadrant(x, y) {
+  if (x == 0 && y == 0) {
+    console.log("Your coordinate is not in a quadrant.");
+  } else {
+    if (x >= 0 && y >= 0) {
+      console.log("Your coodinate lies in quadrant 1.");
+    } else if (x < 0 && y >= 0) {
+      console.log("Your coodinate lies in quadrant 2.");
+    } else if (x < 0 && y < 0) {
+      console.log("Your coodinate lies in quadrant 3.");
+    } else {
+      console.log("Your coodinate lies in quadrant 4.");
+    }
+  }
+}
+
+quadrant(x, y);
+
+// Exercise 4 Sectionp
+console.log("EXERCISE 4:\n==========");
+
+let side1 = prompt("Enter length of the first side of the triangle.");
+let side2 = prompt("Enter length of the second side of the triangle.");
+let side3 = prompt("Enter length of the third side of the triangle.");
+
+function triangleType(side1, side2, side3) {
+  if (side1 == side2 && side1 == side3) {
+    console.log("This is an equilateral triangle.");
+  } else if (side3 <= side1 + side2) {
+    console.log("This is not a valid triangle");
+  } else if (side1 == side2 || side1 == side3) {
+    console.log("This is an isosceles Triangle");
+  } else {
+    console.log("This is a regular triangle.");
+  }
+}
+
+triangleType(side1, side2, side3);
